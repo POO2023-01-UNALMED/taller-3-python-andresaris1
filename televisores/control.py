344dd0c4727bbe17333ea0a2d13bfd898fc1fa37
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-02-26 20:22:47 -0500

    Create control.py

---
+++ televisores/control.py
@@ -0,0 +1,28 @@
+class Control:
+    def __init__(self, tv, turnOn, turnOff, canalUp, canalDown, volumenUp, volumenDown, setCanal, enlazar):
+        self._tv = none
+
+    def canalUp(self):
+        self._tv.canalUp()
+    def canalDown(self):
+        self._tv.canalDown
+
+    def volumenUp(self):
+        self._tv.volumenUp
+    def volumenDown(self):
+        self._tv.volumenDown
+
+    def turnOn(self):
+        self._tv.turnOn
+    def turnOff(self):
+        self._tv.turnOff
+
+    def setcanal(self):
+        self._tv.sercanal
+        
+
+    def enlazar(self):
+        self._tv = tv
+        self._tv-setControl(self)
+
+
