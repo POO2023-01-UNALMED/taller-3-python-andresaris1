6c17d1bb12614554bc36d8c54bd960cc5eb5c631
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-02-26 22:54:18 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -7,7 +7,7 @@ class TV:
         self.precio = 500
         self.estado = estado
         self.volumen = 1
-        self.Control = None
+        self.control = None
         TV.numTV +=1
 
     @classmethod
@@ -72,5 +72,5 @@ class TV:
     def getControl(self):
         return self.control
 
-    def setControl(self, Control):
-        self.Control = control
+    def setControl(self, control):
+        self.control = control
